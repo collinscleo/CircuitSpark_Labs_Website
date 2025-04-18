1fffcd4a327a1ca11f342dc8a5465eb5c38feea8
Cleo Collins <156539179+collinscleo@users.noreply.github.com>
2025-04-18 11:17:19 -0400

    Added a search function

--- Home.js
+++ Home.js
@@ -13,3 +13,31 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+function search() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Check if the input is empty
+    if (filter === "") {
+        // Hide all list items if the input is empty
+        for (i = 0; i < li.length; i++) {
+            li[i].style.display = "none";
+        }
+    } else {
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+}
